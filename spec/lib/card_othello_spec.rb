c8fe111b2a9fa4bb1e62aca893251f6dea76db2e
Tristan Smith <darcnite3000@gmail.com>
2013-04-26 17:36:52 +1000

    started spec-ing out game

--- spec/lib/card_othello_spec.rb
+++ spec/lib/card_othello_spec.rb
@@ -7,3 +7,17 @@ require 'board_cell'
 #   xit 'is a matrix of board_cells' do
 #   end
 # end
+class Game
+  attr_reader :board
+  def initialize(board: nil)
+    @board = board
+  end
+end
+
+describe Game do
+  it "sets up board" do
+    board = double('board')
+    game = Game.new board: board
+    expect(game.board).to eq board
+  end
+end
