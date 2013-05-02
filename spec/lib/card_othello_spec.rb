70e204981905651fe7c527f90087f1ff370214be
Tristan Smith <darcnite3000@gmail.com>
2013-05-02 16:10:50 +1000

    spec-ed out basic card

--- spec/lib/card_othello_spec.rb
+++ spec/lib/card_othello_spec.rb
@@ -1,8 +1,26 @@
 class Card
+  attr_reader :sides, :type, :name
+  def initialize(name: nil, type: nil, sides: {north: 1, south: 1, east: 1, west: 1})
+    @sides = sides
+    @type = type
+    @name = name
+  end
 end
 
 describe Card do
-  xit "has four sides"
-  xit "side value is within the 1-10 range"
-  xit "can tell if it is greater than another card"
+  it "can return it's sides" do
+    points = {north: 1, south: 2, east: 3, west: 4}
+    card = Card.new sides: points
+    expect(card.sides).to eq points
+  end
+  it "can return it's type" do
+    type = double('type')
+    card = Card.new type: type
+    expect(card.type).to eq type
+  end
+  it "can return it's name" do
+    name = "Matz"
+    card = Card.new name: name
+    expect(card.name).to eq name
+  end
 end
